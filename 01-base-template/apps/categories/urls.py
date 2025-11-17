8f08ec2aa5acbfc34dd79d1e74eef97a9618c0d7
padiks <68003199+padiks@users.noreply.github.com>
2025-11-17 18:04:46 +0000

    Delete 01-base-template directory

--- 01-base-template/apps/categories/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='categories:index'),  # This should route to the categories index view
-]
--- 01-base-template/apps/uom/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'home'   # 👈 add this line
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
