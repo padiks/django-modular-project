d42fc39201259c880567eafc147fb5517811589c
padiks <68003199+padiks@users.noreply.github.com>
2025-11-18 11:31:28 +0300

    Delete 04-summation-debug-toolbar directory

--- 04-summation-debug-toolbar/apps/summation/urls.py
+++
@@ -1,9 +0,0 @@
-# apps/summation/urls.py
-# **Guide 2 Step 5: Create app-specific URLs**
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='summation_index'),
-]
