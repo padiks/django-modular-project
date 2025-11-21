9152030d881d2b018ada38455f587d1b72c60fbb
padiks <68003199+padiks@users.noreply.github.com>
2025-11-21 09:51:20 +0300

    Add files via upload

--- 05-foreign-keys-relationships/apps/categories/apps.py
+++ 05-foreign-keys-relationships/apps/categories/apps.py
@@ -1,6 +1,7 @@
 # apps/<app-name>/apps.py
 from django.apps import AppConfig
 
+# Must match with `core/settings.py`
 class CategoriesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.categories' # <-- must match with `core/settings.py`
+    name = 'apps.categories'
