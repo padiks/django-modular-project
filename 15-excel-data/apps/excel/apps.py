b4240b11623043ed80bfc5b64439190f489d87c9
padiks <68003199+padiks@users.noreply.github.com>
2025-11-28 15:12:15 +0300

    Add files via upload

---
+++ 15-excel-data/apps/excel/apps.py
@@ -0,0 +1,7 @@
+# apps/excel/apps.py
+from django.apps import AppConfig
+
+# Must match with `core/settings.py`
+class ExcelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.excel'
